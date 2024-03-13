aef4f3279052d72397bbdd6de94c3df960559090
Rustamli510 <qedir.rustemli02@gmail.com>
2024-03-13 18:06:31 +0400

    home page media

--- Starbucks/src/Header.jsx
+++ Starbucks/src/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './scss/Header.css'
 import { NavLink } from 'react-router-dom'
 
 function Header() {
+
+    const [clicked, setClicked] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const Handleclick = () => {
+        setClicked(!clicked)
+        setOpen(!open)
+    }
     return (
         <div>
             <header>
@@ -21,6 +29,32 @@ function Header() {
                     </div>
                     <a className="sign" href="">Sign in</a>
                     <a href='' className='join'>Join now</a>
+                </div>
+                <div className={`media ${clicked ? 'clicked' : ''}`} onClick={Handleclick}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+                <div className={`sidebar ${open ? 'open' : ''}`}>
+                    <div className="top">
+                        <ul>
+                            <li><a href="">Menu</a></li>
+                            <li><a href="">Rewards</a></li>
+                            <li><a href="">Gift Cards</a></li>
+                        </ul>
+                        <hr />
+                    </div>
+                    <div className="bottom">
+                        <div className="bottom-top">
+                            <a className="sign" href="">Sign in</a>
+                            <a href='' className='join'>Join now</a>
+                        </div>
+                        <div className="bottom-bottom">
+                            <svg aria-hidden="true" className="valign-middle pr2" focusable="false" preserveAspectRatio="xMidYMid meet" style={{ width: '21px', height: '21px', overflow: 'visible', fill: 'currentColor' }} viewBox="0 0 24 24"><path d="M12,11.475 C10.5214286,11.475 9.32142857,10.299 9.32142857,8.85 C9.32142857,7.401 10.5214286,6.225 12,6.225 C13.4785714,6.225 14.6785714,7.401 14.6785714,8.85 C14.6785714,10.299 13.4785714,11.475 12,11.475 M12,1.5 C7.85357143,1.5 4.5,4.7865 4.5,8.85 C4.5,14.3625 12,22.5 12,22.5 C12,22.5 19.5,14.3625 19.5,8.85 C19.5,4.7865 16.1464286,1.5 12,1.5"></path></svg>
+                            <a href=''>Find a store</a>
+                        </div>
+
+                    </div>
                 </div>
             </header>
         </div>
