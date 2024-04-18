2032412a2f16d2a2262c66718229755e8b8e2053
Rustamli510 <qedir.rustemli02@gmail.com>
2024-04-18 16:07:04 +0400

    coffee inner page

--- Starbucks/src/CoffeeInner.jsx
+++ Starbucks/src/CoffeeInner.jsx
@@ -7,12 +7,27 @@ import Venti from '../img/55e7819f7cf8e1959ec35e680d46d9a9.svg'
 import Grande from '../img/2920fb2a8c34d3ddb95ad262872526e9.svg'
 import Trenta from '../img/3abf3fc78365ef0b59bbfd0ecd1c8490.svg'
 import Loc from '../img/location_2838912.png'
+import Star from '../img/wizard_1803305.png'
+import Info from '../img/info_471662.png'
 
 function CoffeeInner() {
 
   const params = useParams()
   console.log(params);
   const [coffeeData, setCoffeeData] = useState(null)
+  const [count, setCount] = useState(4);
+
+  const increment = () => {
+    if (count < 12) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
 
   const getCoffee = () => {
     axios.get(`http://localhost:3000/coffee/${params.id}`).then(res => {
@@ -104,6 +119,64 @@ function CoffeeInner() {
             <img src={Loc} alt="" />
             <p>Select a store to view availability</p>
           </div>
+        </div>
+
+        <div className="right">
+          <div className="whats-included">
+            <h1>What's included</h1>
+            <div className="size-bottom"></div>
+          </div>
+
+          <div className="right-center">
+            <div>
+              <select>
+                <fieldset><legend>Ads</legend></fieldset>
+                <option >Extra water</option>
+                <option >Light water</option>
+                <option >No water</option>
+                <option >Water</option>
+              </select>
+
+              <select>
+                <option>Espresso &amp; Shot Options</option>
+                <option>Signature Espresso Roast</option>
+                <option>Blonde Espresso Roast</option>
+                <option>Decaf Espresso Roast</option>
+                <option>1/3 Decaf Espresso Roast</option>
+                <option>1/2 Decaf Espresso Roast</option>
+                <option>2/3 Decaf Espresso Roast</option>
+              </select>
+
+              <div className="shots">
+                <div className="label">
+                  <label>Espresso &amp; Shot Options</label>
+                </div>
+                <div className="left">
+                  <p>Shots</p>
+                </div>
+                <div className="count">
+                  <button onClick={decrement} style={{ visibility: count === 1 ? 'hidden' : 'visible' }}>-</button>
+                  <h2>{count}</h2>
+                  <button onClick={increment} style={{ visibility: count === 12 ? 'hidden' : 'visible' }}>+</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="customize"> 
+            <button><img src={Star} alt="" />Customize</button>
+          </div>
+        </div>
+      </section>
+
+      <section className='nutrition'>
+        <p className="item">
+          200★ item
+        </p>
+
+        <div className="center">
+          <p>Espresso shots topped with hot water create a light layer of <br /> crema culminating in this wonderfully rich cup with depth and <br /> nuance.</p>
+          <span>15 calories, 0g sugar, 0g fat</span>
         </div>
       </section>
     </div>
